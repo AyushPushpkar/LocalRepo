b373ad1e2fae72a2cb6cbd6aefbd4c955999a2e5
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2025-03-14 13:12:33 +0530

    basics js

---
+++ js-basics/var.js
@@ -0,0 +1,18 @@
+let name = "Ayush";
+console.log(name);
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+const pi = 3.14;
+console.log(pi);
+
+let firstName = undefined;
+let selectedColor = null;
+console.log(typeof selectedColor);
+
+console.log(typeof name);
+
+name = 390;
+console.log(typeof name);
